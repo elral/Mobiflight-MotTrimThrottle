91adf63fd697895192f1f6b7b464c293b950e81f
elral <3263285+elral@users.noreply.github.com>
2022-03-15 17:16:35 +0100

    changed pins

--- include/configuration.h
+++ include/configuration.h
@@ -4,9 +4,25 @@ const char configuration[] PROGMEM =
     "3.7.enableThrottle:"
     "11.14.5.TrimWheel:"
     "11.15.5.Throttle:"
+    "4.8.8.8.15.1.TrimSet:"
+    "4.9.9.9.15.1.ThrottleSet:"
+    "1.10.SyncLostTrim:"
+    "1.11.SyncLostThrottle:"
+};
+
+/*
+const char configuration[] PROGMEM = 
+{   "3.4.enableTrim:"
+    "3.7.enableThrottle:"
+    "11.14.5.TrimWheel:"
+    "11.15.5.Throttle:"
     "4.8.10.9.15.1.TrimSet:"
     "4.11.13.12.15.1.ThrottleSet:"
+    "1.12.SyncLostTrim:"
+    "1.13.SyncLostThrottle:"
 };
+*/
+
 /*
 const char configuration[] PROGMEM = 
 {   "3.4.enableTrim:"
